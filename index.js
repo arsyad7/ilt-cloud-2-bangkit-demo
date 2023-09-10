46b71fdd13efd764de78bfe5cb7079ce7b2d6716
arsyad <arsyadrama7@gmail.com>
2023-09-10 20:03:22 +0700

    add env

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes/contact');
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: process.env.PORT,
+        host: process.env.HOST
     });
 
     server.route(routes);
